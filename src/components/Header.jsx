349f8b6e6ee8e1ea1cefc45b95dc77c482cfbae3
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2023-12-14 23:52:58 +0530

    Added Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
 
 const Header = () => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate('/')
   }
+
+  const {currency,setCurrency } = CryptoState();
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 border-slate-950 border rounded-full border-3 flex justify-center p-5 mt-5 w-full cursor-pointer">
         <div className="flex-1" onClick={handleClick}>
-          <a className="btn btn-ghost text-3xl text-lime-400">CoinCanvas</a>
+          <a className="btn btn-ghost text-5xl text-lime-400 :hover rounded-full :hover btn-wide">
+            CoinCanvas
+          </a>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1 text-2xl text-white mr-8">
-            <li>
-              <details>
-                <summary>Currency</summary>
-                <ul className="p-2 bg-slate-900 rounded-t-none">
-                  <li className="px-9 py-3" value={"USD"}>USD</li>
-                  <li className="px-9" value={"INR"}>INR</li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
+        <select className="select select-bordered mr-12 text-white rounded-full text-xl" value={currency} onChange={(e) => setCurrency(e.target.value)}>
+          <option disabled selected className='text-white'>
+            Currency
+          </option>
+          <option value={'USD'}>USD</option>
+          <option value={'INR'}>INR</option>
+        </select>
       </div>
     </div>
   );
